dc780413d1c36638ad23ef0b1cb448c03e961bfb
Jhun24 <jjanghuni102@naver.com>
2017-11-17 09:17:22 +0900

    bug fix

--- routes/room.js
+++ routes/room.js
@@ -307,7 +307,7 @@ function room(app , userModel , roomModel , acceptRoomModel , randomString , fri
                     }
                     else{
                         var userDistance = model[0]["user1Distance"] + data.userDistance;
-                        var goalCredit = model[0]["goalCredit"];
+                        var goalCredit = parseInt(model[0]["awardCredit"]);
 
                         if(userDistance >= model[0]["goalDistance"]){
                             friendModel.find({"token":data.token},(err,model)=>{
